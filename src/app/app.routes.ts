3c74315affd5a2983878a1f4c0f66aef23fb5431
Raiyaan Mohammed <raiyaanmohammed15@gmail.com>
2026-09-12 18:06:56 +0530

    Complete frontend battle UI and navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,26 @@
 import { Routes } from '@angular/router';
-import { Register } from './register/register';
+import { Home } from './home/home';
 import { Quiz } from './quiz/quiz';
+import { Leaderboard } from './leaderboard/leaderboard';
+import { History } from './history/history'; 
+import { Register } from './register/register';
 
 export const routes: Routes = [
   {
     path: '',
+    component: Home
+  },
+  {
+    path: 'battle',
     component: Quiz
+  },
+  {
+    path: 'leaderboard',
+    component: Leaderboard
+  },
+  {
+    path: 'history',
+    component: History 
   },
   {
     path: 'register',
---
+++ src/app/history/history.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-history',
+  styleUrl: './history.css',
+  templateUrl: './history.html',
+})
+export class History {}
