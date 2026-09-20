0abfc4d5645d02e2cccc782eb6a4baf37717e44b
Raiyaan Mohammed <raiyaanmohammed15@gmail.com>
2026-09-20 21:34:26 +0530

    Fix production CORS

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ connectDB();
 // ======================================================
 
 app.use(cors({
-  origin: 'http://localhost:4200',
+  origin: 'https://quiz-battle-arena-1sz5q3ah9-raiyaan.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
